313a82abc25758bd02e9f2bd5b209bfc954d4251
Calin Nicoara <calin.nicoara@esolutions.ro>
2020-03-22 15:39:24 +0200

    Added new classes for using fields and methods.

---
+++ src/java/ro/cni/course/deutschebank/prepared/reflection/ReflectionFieldsMain.java
@@ -0,0 +1,47 @@
+package ro.cni.course.deutschebank.prepared.reflection;
+
+import java.lang.reflect.Field;
+
+public class ReflectionFieldsMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        client.setName("Denis");
+        client.setLastName("The Menice");
+
+        Class<?> aClass = client.getClass();
+
+        try {
+            final Field nameField = aClass.getDeclaredField("name");
+            final Object o = nameField.get(client);
+            System.out.println(o);
+
+            nameField.set(client, "Cornel");
+            System.out.println(client.getName());
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Client {
+
+        private String name;
+        String lastName;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(final String name) {
+            this.name = name;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(final String lastName) {
+            this.lastName = lastName;
+        }
+    }
+}
---
+++ src/java/ro/cni/course/deutschebank/prepared/reflection/ReflectionMethodsMain.java
@@ -0,0 +1,45 @@
+package ro.cni.course.deutschebank.prepared.reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectionMethodsMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        client.setName("Denis");
+        client.setLastName("The Menice");
+
+        Class<?> aClass = client.getClass();
+
+        try {
+            final Method setNameMethod = aClass.getMethod("setName", String.class);
+            setNameMethod.invoke(client, "The Good boy");
+            System.out.println(client.getName());
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Client {
+
+        private String name;
+        String lastName;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(final String name) {
+            this.name = name;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(final String lastName) {
+            this.lastName = lastName;
+        }
+    }
+}
