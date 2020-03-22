a86e2c383cf5eb7be10e7f01a39308314200c3f6
Calin Nicoara <calin.nicoara@esolutions.ro>
2020-03-22 14:06:38 +0200

    Added reflection proxy example.

---
+++ src/java/ro/cni/course/deutschebank/day6/reflection/ProxyMain.java
@@ -0,0 +1,47 @@
+package ro.cni.course.deutschebank.day6.reflection;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProxyMain {
+    public static void main(String[] args) {
+        Map<String, String> myMap = new HashMap<>();
+        myMap.put("hello", "there");
+
+        final Map<String, String> proxyMap = (Map) Proxy.newProxyInstance(
+                ProxyMain.class.getClassLoader(),
+                new Class[]{Map.class},
+                new MyInvocationHandler(myMap)
+        );
+
+        proxyMap.put("test", "this");
+
+        System.out.println(proxyMap.get("test"));
+    }
+}
+
+class MyInvocationHandler implements InvocationHandler {
+
+    private Map target;
+
+    public MyInvocationHandler(Map target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+        System.out.println("Proxy used");
+        System.out.println(method.getName());
+        for(Object arg: args) {
+            System.out.println("Argument: " +  arg);
+        }
+
+        final Object invoke = method.invoke(target, args);
+
+        return invoke;
+    }
+}
+
