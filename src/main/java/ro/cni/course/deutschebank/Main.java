cb6e32dbefb3c0c7ed3a5d1c5fa3d3a3785177f9
Calin Nicoara <calin.nicoara@esolutions.ro>
2020-04-02 12:57:44 +0300

    Removed.

--- src/main/java/ro/cni/course/deutschebank/Main.java
+++ src/main/java/ro/cni/course/deutschebank/Main.java
@@ -2,6 +2,6 @@ package ro.cni.course.dbcourse;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Deutsche Bank!");
+        System.out.println("Hello, People!");
     }
 }
